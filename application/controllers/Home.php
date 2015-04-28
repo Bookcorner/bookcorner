486135f1f44f988c67ed89d1d37b5108d54d4ded
Antonio Ortiz <prueba@prueba.com>
2015-04-28 11:10:25 +0200

    Añadidas carpetas dentro de templetes para clasificarlas

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -3,8 +3,8 @@ defined ( 'BASEPATH' ) or exit ( 'No direct script access allowed' );
 class Home extends CI_Controller {
 	public function index() {
 		$data['title'] = 'Home';
-		$this->load->view ( 'templates/cabecera', $data);
-		$this->load->view ( 'templates/menu_login' );
+		$this->load->view ( 'templates/cabeceras/cabecera_base', $data);
+		$this->load->view ( 'templates/menus/menu_login' );
 		$this->load->view ( 'templates/end' );
 	}
 }
