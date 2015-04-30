bb0402eb78e7c12c078f69598bd6f7d005e94511
Antonio Ortiz <prueba@prueba.com>
2015-04-30 09:52:20 +0200

    Cambio del buscador para que aparezca pegado al botón de registro.

--- application/views/templates/menus/menu_login.php
+++ application/views/templates/menus/menu_login.php
@@ -1,7 +1,6 @@
-<!--HEADER-->
+<!--CONTAINER-->
 <div class="container">
-
-	<!--Comienza el menú header principal-->
+	<!--MENU-->
 	<div class="row">
 		<div class="col-xs-12">
 			<nav class="navbar navbar-default" role="navigation">
@@ -12,26 +11,26 @@
 							class="icon-bar"></span> <span class="icon-bar"></span> <span
 							class="icon-bar"></span>
 					</button>
-					<a class="navbar-brand" href="#">Bootstrap</a>
+					<a class="navbar-brand" href="home">BookCorner</a>
 				</div>
 				<ul class="nav navbar-nav">
 					<li class="active"><a href="#">Home</a></li>
-					<li><a href="#about">Elemento 1</a></li>
-					<li><a href="#about">Elemento 2</a></li>
-					<li><a href="#about">Elemento 3</a></li>
+					<li><a href="#about">Libros</a></li>
+					<li><a href="#about">Autores</a></li>
 				</ul>
 				<div class="collapse navbar-collapse"
 					id="bs-example-navbar-collapse-1">
-					<form class="navbar-form navbar-left" role="search">
-						<div class="form-group">
-							<input type="text" class="form-control" placeholder="Buscar">
-						</div>
-						<button type="submit" class="btn btn-default">
-							<i class="glyphicon glyphicon-search"></i>
-						</button>
-					</form>
-
 					<ul class="nav navbar-nav navbar-right">
+						<li>
+							<form class="navbar-form navbar-left" role="search">
+								<div class="form-group">
+									<input type="text" class="form-control" placeholder="Buscar...">
+								</div>
+								<button type="submit" class="btn btn-default">
+									<i class="glyphicon glyphicon-search"></i>
+								</button>
+							</form>
+						</li>
 						<li><a href="#" data-toggle="modal" data-target="#myModal">Registrarse</a></li>
 						<!-- Modal -->
 						<div class="modal fade" id="myModal" tabindex="-1" role="dialog"
@@ -48,110 +47,110 @@
 									</div>
 									<div class="modal-body">
 										<div class="col-xs-12">
-											<?php echo form_open ( '#', [
-														'class' => 'form-horizontal',
-														'data-toggle' => 'validator',
-														'method' => 'post',
-														'accept-charset' => 'UTF-8',
-														'id' => 'idFormSignIn' 
-												] )?>
+											<?php
+											echo form_open ( '#', [ 
+													'class' => 'form-horizontal',
+													'data-toggle' => 'validator',
+													'method' => 'post',
+													'accept-charset' => 'UTF-8',
+													'id' => 'idFormSignIn' 
+											] )?>
 												<fieldset>
-													<!-- Name input-->
-													<div class="form-group">
-														<label class="control-label" for="name">Nombre:</label>
-														<div class="controls">
-															<input id="name" name="name" class="form-control"
-																type="text" placeholder="Nombre" class="input-large"
-																required>
-														</div>
-														<div class="help-block with-errors"></div>
+												<!-- Name input-->
+												<div class="form-group">
+													<label class="control-label" for="name">Nombre:</label>
+													<div class="controls">
+														<input id="name" name="name" class="form-control"
+															type="text" placeholder="Nombre" class="input-large"
+															required>
 													</div>
+													<div class="help-block with-errors"></div>
+												</div>
 
-													<div class="form-group">
-														<label class="control-label" for="surname">Apellido:</label>
-														<div class="controls">
-															<input id="surname" name="surname" class="form-control"
-																type="text" placeholder="Apellido" class="input-large"
-																required>
-														</div>
-														<div class="help-block with-errors"></div>
+												<div class="form-group">
+													<label class="control-label" for="surname">Apellido:</label>
+													<div class="controls">
+														<input id="surname" name="surname" class="form-control"
+															type="text" placeholder="Apellido" class="input-large"
+															required>
 													</div>
+													<div class="help-block with-errors"></div>
+												</div>
 
-													<!-- Username input-->
-													<div class="form-group">
-														<label class="control-label" for="user">Nombre de usuario:</label>
-														<div class="controls">
-															<input id="user" name="user" class="form-control"
-																type="text"
-																placeholder="Usuario (tu nombre visible en la página)"
-																class="input-large" required>
-														</div>
-														<div class="help-block with-errors"></div>
+												<!-- Username input-->
+												<div class="form-group">
+													<label class="control-label" for="user">Nombre de usuario:</label>
+													<div class="controls">
+														<input id="user" name="user" class="form-control"
+															type="text"
+															placeholder="Usuario (tu nombre visible en la página)"
+															class="input-large" required>
 													</div>
+													<div class="help-block with-errors"></div>
+												</div>
 
-													<!-- Password input-->
-													<div class="form-group">
-														<label class="control-label" for="pass">Contraseña:</label>
-														<div class="controls">
-															<input id="pass" name="pass" class="form-control" pattern="^{5,12}$"
-																type="password" placeholder="********"
-																data-error="Contraseña no válida"
-																class="input-large" required>
-																
-														</div>
-														<div class="help-block with-errors"></div>
-													</div>
+												<!-- Password input-->
+												<div class="form-group">
+													<label class="control-label" for="pass">Contraseña:</label>
+													<div class="controls">
+														<input id="pass" name="pass" class="form-control"
+															pattern="^{5,12}$" type="password" placeholder="********"
+															data-error="Contraseña no válida" class="input-large"
+															required>
 
-													<div class="form-group">
-														<label class="control-label" for="repass">Introduce
-															contraseña de nuevo:</label>
-														<div class="controls">
-															<input id="repass" name="repass" data-match="#pass"
-																class="form-control" type="password"
-																placeholder="********" class="input-large"
-																data-match-error="Error, la contraseña no coincide"
-																required>
-														</div>
-														<div class="help-block with-errors"></div>
 													</div>
+													<div class="help-block with-errors"></div>
+												</div>
 
-													<!-- Email input-->
-													<div class="form-group">
-														<label class="control-label" for="email">Email:</label>
-														<div class="controls">
-															<input id="email" name="email"
-																pattern="^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$"
-																class="input-large form-control" type="text"
-																placeholder="email"
-																data-error="Dirección de correo no válida"
-																required>
-														</div>
-														<div class="help-block with-errors"></div>
+												<div class="form-group">
+													<label class="control-label" for="repass">Introduce
+														contraseña de nuevo:</label>
+													<div class="controls">
+														<input id="repass" name="repass" data-match="#pass"
+															class="form-control" type="password"
+															placeholder="********" class="input-large"
+															data-match-error="Error, la contraseña no coincide"
+															required>
 													</div>
+													<div class="help-block with-errors"></div>
+												</div>
 
-													<div class="form-group">
-														<label class="control-label" for="reemail">Introduce email
-															de nuevo:</label>
-														<div class="controls">
-															<input id="reemail" name="reemail" data-match="#email"
-																class="input-large form-control" type="text"
-																placeholder="email"
-																data-match-error="Error, el email no coincide" required>
-														</div>
-														<div class="help-block with-errors"></div>
+												<!-- Email input-->
+												<div class="form-group">
+													<label class="control-label" for="email">Email:</label>
+													<div class="controls">
+														<input id="email" name="email"
+															pattern="^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$"
+															class="input-large form-control" type="text"
+															placeholder="email"
+															data-error="Dirección de correo no válida" required>
 													</div>
+													<div class="help-block with-errors"></div>
+												</div>
 
-													<!-- Button -->
-													<div class="form-group">
-														<label class="control-label" for="confirmsignup"></label>
-														<div class="controls">
-															<button id="confirmsignup" name="confirmsignup"
-																class="btn btn-success">Registrar</button>
-															<button type="button" class="btn btn-danger"
-																data-dismiss="modal">Cerrar</button>
-														</div>
+												<div class="form-group">
+													<label class="control-label" for="reemail">Introduce email
+														de nuevo:</label>
+													<div class="controls">
+														<input id="reemail" name="reemail" data-match="#email"
+															class="input-large form-control" type="text"
+															placeholder="email"
+															data-match-error="Error, el email no coincide" required>
 													</div>
-												</fieldset>
+													<div class="help-block with-errors"></div>
+												</div>
+
+												<!-- Button -->
+												<div class="form-group">
+													<label class="control-label" for="confirmsignup"></label>
+													<div class="controls">
+														<button id="confirmsignup" name="confirmsignup"
+															class="btn btn-success">Registrar</button>
+														<button type="button" class="btn btn-danger"
+															data-dismiss="modal">Cancelar</button>
+													</div>
+												</div>
+											</fieldset>
 											</form>
 										</div>
 									</div>
@@ -169,7 +168,7 @@
 											<div><?php echo validation_errors()?></div>
 											<div><?php echo $this->session->flashdata('error')?></div>
 												<?php
-												echo form_open ( 'login/signin', [
+												echo form_open ( 'login/signin', [ 
 														'class' => 'form',
 														'role' => 'form',
 														'method' => 'post',
@@ -177,35 +176,35 @@
 														'id' => 'idFormLogin' 
 												] )?>
 												<div class="form-group">
-													<label class="sr-only" for="emailLogin">Usuario</label> <input
-														type="text" class="form-control" id="emailLogin"
-														placeholder="Usuario" required>
-												</div>
-												<div class="form-group">
-													<label class="sr-only" for="passLogin">Contraseña</label> <input
-														type="password" class="form-control" id="passLogin"
-														placeholder="Contraseña" required>
-												</div>
-												<div class="checkbox">
-													<label> <input type="checkbox"> Recordar
-													</label>
-												</div>
-												<div class="form-group">
-													<button type="submit" class="btn btn-success btn-block">Acceder</button>
-												</div>
+												<label class="sr-only" for="emailLogin">Usuario</label> <input
+													type="text" class="form-control" id="emailLogin"
+													placeholder="Usuario" required>
+											</div>
+											<div class="form-group">
+												<label class="sr-only" for="passLogin">Contraseña</label> <input
+													type="password" class="form-control" id="passLogin"
+													placeholder="Contraseña" required>
+											</div>
+											<div class="checkbox">
+												<label> <input type="checkbox"> Recordarme
+												</label>
+											</div>
+											<div class="form-group">
+												<button type="submit" class="btn btn-success btn-block">Acceder</button>
+											</div>
 											</form>
 										</div>
 									</div>
 								</li>
-							</ul>
-						</li>
+							</ul></li>
 					</ul>
 				</div>
 			</nav>
 		</div>
 	</div>
 </div>
-
+<!-- SCRIPTS -->
 <script>
 $('#myForm').validator()
 </script>
+<!-- SCRIPTS -->
