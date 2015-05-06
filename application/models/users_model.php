869c52dc33981b9b9ee574a9d33955515474e0d1
Antonio Ortiz <prueba@prueba.com>
2015-05-06 18:22:09 +0200

    Añadido prototipo para hacer test a controladores Añadido constructor en el modelo de los usuarios

--- application/models/users_model.php
+++ application/models/users_model.php
@@ -1,8 +1,15 @@
 <?php
 defined ( 'BASEPATH' ) or exit ( 'No direct script access allowed' );
 class Users_model extends CI_Model {
+	
+	function __construct() {
+		parent::__construct ();
+	}
+	
 	function check_valid_user($username) {
-		$userBean = R::findOne ( 'user', ' user_nickname = ? ', [$username] );
+		$userBean = R::findOne ( 'user', ' user_nickname = ? ', [ 
+				$username 
+		] );
 		return $userBean;
 	}
 }
