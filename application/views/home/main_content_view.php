b1e1d6c51c95fa75666858769b7ce92fdc4d23ca
Antonio Ortiz <prueba@prueba.com>
2015-05-07 14:42:02 +0200

    Añadido el texto de las páginas estáticas, con instrucciones de diseño front-end

--- application/views/home/main_content_view.php
+++ application/views/home/main_content_view.php
@@ -1,1 +1,9 @@
-<h1>Main Content view</h1>
+<div style="left: 400px;">
+	<p>Aquí va la introducción de la página, dic iendo que es un portal
+		para hacer leer y compartir libros, y llevar una lista organizada de
+		los mismos</p>
+</div>
+<div style="width: 200px;float: right;">
+	<p>En los laterales van dos recuadros en plan libros más leidos, libros
+		recien añadidos, autores más leidos, autores recien añadidos</p>
+</div>
