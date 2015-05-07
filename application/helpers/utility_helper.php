59740e19400eb5f05a76dd9dd6dcba50275ddde0
Antonio Ortiz <prueba@prueba.com>
2015-05-08 01:01:44 +0200

    añadido el helper para formar vistas básicas con un solo contenido

--- application/helpers/utility_helper.php
+++ application/helpers/utility_helper.php
@@ -13,3 +13,23 @@ function check_cookie_or_session_exist() {
 	
 	return false;
 }
+
+/**
+ * Carga las vistas principales de la página, más la vista del contenido, que es pasada por parámetro.
+ * 
+ * @param String $contentURI uri que contiene la ruta del contenido principal
+ * @param Array $data contiene los datos que vana  ser pasados a las vistas
+ */
+function loadBasicViews($contentURI, $data) {
+	$CI =& get_instance();
+	$CI->load->view ( 'templates/cabeceras/cabecera_base', $data );
+
+	if (check_cookie_or_session_exist ()) {
+		$CI->load->view ( 'templates/menus/menu_logout' );
+	} else {
+		$CI->load->view ( 'templates/menus/menu_login', $data );
+	}
+	$CI->load->view ( $contentURI, $data );
+	$CI->load->view ( 'templates/footers/base_footer' );
+	$CI->load->view ( 'templates/end' );
+}
