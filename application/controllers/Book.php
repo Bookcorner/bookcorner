7c230e8efbbd598418f40083ef10aab1eba61536
Antonio Ortiz <prueba@prueba.com>
2015-05-08 23:17:01 +0200

    Añadido plugin de grocery crud. Añadida funcionalidad del crudo de libros. URL de prueba: http://localhost/bookcorner/book/bookscrud

--- application/controllers/Book.php
+++ application/controllers/Book.php
@@ -1,6 +1,14 @@
 <?php
 defined ( 'BASEPATH' ) or exit ( 'No direct script access allowed' );
 class Book extends CI_Controller {
+    public function __construct() {
+        parent::__construct ();
+        
+        $this->load->database ();
+        $this->load->helper ( 'url' );
+        
+        $this->load->library ( 'grocery_CRUD' );
+    }
     public function index() {
         $data ['title'] = 'Libros';
         $viewUri = 'books/main_book_content';
@@ -18,6 +26,21 @@ class Book extends CI_Controller {
             $data ['books'] = $books;
             $viewUri = 'books/list_of_books';
             loadBasicViews ( $viewUri, $data );
+        }
+    }
+    public function bookscrud() {
+        try {
+            $crud = new grocery_CRUD ();
+            
+            $crud->set_table ( 'book' );
+            $crud->set_subject ( 'Libros' );
+            $crud->required_fields ( 'book_id' );
+            $crud->columns ( 'book_id', 'book_isbn', 'book_name', 'book_desc', 'book_img' );
+            
+            $output = $crud->render ();
+            $this->load->view ( 'books/booksCRUD', $output );
+        } catch ( Exception $e ) {
+            show_error ( $e->getMessage () . ' --- ' . $e->getTraceAsString () );
         }
     }
 }
