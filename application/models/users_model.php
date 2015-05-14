31760cb35da221651a20e5cceb69c8af2ec9abc5
Mario <mcantelar@hotmail.es>
2015-05-14 11:52:23 +0200

    creacion registro

--- application/models/users_model.php
+++ application/models/users_model.php
@@ -1,19 +1,40 @@
 <?php
 defined ( 'BASEPATH' ) or exit ( 'No direct script access allowed' );
+
 class Users_model extends CI_Model {
+    
     public function __construct() {
         parent::__construct ();
     }
+    
     public function check_valid_user($username) {
         $userBean = R::findOne ( 'user', ' user_nickname = ? ', [ 
                 $username 
         ] );
         return $userBean;
     }
+    
     function getUserInfo($userId) {
         $userBean = R::findOne ( 'user', ' user_id = ? ', [ 
                 $userId
         ] );
         return $userBean;
     }
+    
+    function check_exist_user($username, $email) {
+        $countUser = R::count( 'user', ' user_nickname = ? ', [ $username ] );
+        
+        if ($countUser != 0) {
+            return 1;
+        }
+                
+        $countUser = R::count( 'user', ' user_email = ? ', [ $email ] );
+        
+        if ($countUser != 0) {
+            return 2;
+        }
+        
+        return 0;
+    }
+    
 }
