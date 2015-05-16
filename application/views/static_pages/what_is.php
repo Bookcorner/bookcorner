f5815aa445eccf6231b097eec3ade2940a71bb62
Juan Antonio Ortiz <juananortizc@gmail.com>
2015-05-16 14:33:10 +0200

    cambio página estática ¿qué es bookcorner?

--- application/views/static_pages/what_is.php
+++ application/views/static_pages/what_is.php
@@ -1,28 +1,141 @@
-<div class="col-xs-12">
-	<div class="col-xs-6 col-xs-offset-2">
-		<h2>¿Qué es Bookcorner?</h2>
-		<p>Bookcorner es un sitio web dedicado única y exclusivamente a los
-			libros.</p>
-		<p>
-			Pretende ser un lugar donde los amantes de los mismos puedan tener un
-			<strong>registro organizado de todos los libros</strong> que han
-			leido o quieren leer, así como poder conocer las tendencias
-			principales de libros de la actualidad
-		</p>
-		<p>
-			Está en <strong>crecimiento constante: </strong>dado que los libros
-			no paran de salir, es (y será) frecuente que el sitio no disponga del
-			libro que quieres añadir a tu lista en este momento. Debido a la
-			cantidad ingente de libros que hay, os pedimos vuestra ayuda para
-			seguir aumentando el contenido del sitio, pudiendo vosotros enviar la
-			información de ese libro o de ese autor que tanto os gusta y que no
-			está registrado todavía.
-		</p>
-		<p>
-			Tampoco queremos aburriros con las características que tiene, o la
-			visión a futuro del sitio, os dejamos la parte de investigación.
-			Recordad que si teneis alguna sugerencia, siempre será bienvenida <a
-				href="#">aquí</a>
-		</p>
+<!-- START THE FEATURETTES -->
+<div class="container">
+	<!-- CAROUSEL -->
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+      <!-- Indicators -->
+      <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+        <li data-target="#myCarousel" data-slide-to="3"></li>
+      </ol>
+      <div class="carousel-inner" role="listbox">
+        <div class="item active">
+          <img class="first-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="First slide">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>¿Qué es Bookcorner?.</h1>
+              <p>Te damos la bienvenida a Bookcorner: Lugar de gestión de tus libros y autores preferidos.</p>
+              <p><a class="btn btn-lg btn-primary" href="#" role="button">Ver más</a></p>
+            </div>
+          </div>
+        </div>
+        <div class="item">
+          <img class="second-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Second slide">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>Actualiza tu lista de libros.</h1>
+              <p>Gestiona en un lugar centralizado todos los libros que te has leído, y compártelos con tus amigos de forma sencilla.</p>
+              <p><a class="btn btn-lg btn-primary" href="#" role="button">¡Quiero Gestionar mis listas!</a></p>
+            </div>
+          </div>
+        </div>
+        <div class="item">
+          <img class="third-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Third slide">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>Continuo crecimiento.</h1>
+              <p>El contenido dinámico de bookcorner permite que el libro que no encuentres hoy, esté mañana disponible. Para que el sitio crezca, es necesaria tu ayuda. ¿Nos echas un cable?</p>
+              <p><a class="btn btn-lg btn-primary" href="#" role="button">¡Quiero ayudar!</a></p>
+            </div>
+          </div>
+        </div>
+        <div class="item">
+          <img class="forth-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Third slide">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>Vota a tus autores preferidos.</h1>
+              <p>Comparte tus gustos y opiniones con el resto de la comunidad por medio de los votos de libros y autores</p>
+              <p><a class="btn btn-lg btn-primary" href="#" role="button">¡Quiero votar!</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+	<!-- FIN CAROUSEL -->
+	<div class="row">
+		<hr class="featurette-divider">
+		<div class="row featurette">
+			<div class="col-md-7">
+				<h2 class="featurette-heading">
+					Bookcorner <span class="text-muted"> dedicado única y
+						exclusivamente a los libros. </span>
+				</h2>
+				<p class="lead">
+					Pretende ser un lugar donde los amantes de los mismos puedan tener
+					un <strong>registro organizado de todos los libros</strong> que han
+					leido o quieren leer, así como poder conocer las tendencias
+					principales de libros de la actualidad.
+				</p>
+			</div>
+			<div class="col-md-5">
+				<img class="featurette-image img-responsive center-block"
+					data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+			</div>
+		</div>
+
+		<hr class="featurette-divider">
+
+		<div class="row featurette">
+			<div class="col-md-7 col-md-push-5">
+				<h2 class="featurette-heading">Actualiza tus listas.</h2>
+				<p class="lead">Bookcorner pretende ser un sitio en el que puedas
+					organizar y compartir tus libros preferidos con la comunidad.</p>
+				<p class="lead">Para ello, te proveemos de la gestión total de los
+					libros que estás leyendo actualmente, pudiendo poner notas sobre
+					qué te parecieron, las páginas que te gustaría recordar de los
+					mismos y apuntes de cualquier índole.</p>
+			</div>
+			<div class="col-md-5 col-md-pull-7">
+				<img class="featurette-image img-responsive center-block"
+					data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+			</div>
+		</div>
+
+		<hr class="featurette-divider">
+
+		<div class="row featurette">
+			<div class="col-md-7">
+				<h2 class="featurette-heading">
+					Crecimiento Constante y Colectivo. <span class="text-muted">Ayuda a
+						la página a crecer.</span>
+				</h2>
+				<p class="lead">Dado que los libros no paran de salir, es (y será)
+					frecuente que el sitio no disponga del libro que quieres añadir a
+					tu lista en este momento. Debido a la cantidad ingente de libros
+					que hay, os pedimos vuestra ayuda para seguir aumentando el
+					contenido del sitio, pudiendo vosotros enviar la información de ese
+					libro o de ese autor que tanto os gusta y que no está registrado
+					todavía.</p>
+			</div>
+			<div class="col-md-5">
+				<img class="featurette-image img-responsive center-block"
+					data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+			</div>
+		</div>
+
+		<hr class="featurette-divider">
+
+		<div class="row featurette">
+			<div class="col-md-7 col-md-push-5">
+				<h2 class="featurette-heading">Vota tus preferidos.</h2>
+				<p class="lead">Bookcorner te la posibilidad de votar sobre los
+					autores y libros de la página, pudiendo así conocer la popularidad
+					de cada uno e inevitablemente descubriendo nuevos libros y autores</p>
+			</div>
+			<div class="col-md-5 col-md-pull-7">
+				<img class="featurette-image img-responsive center-block"
+					data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+			</div>
+		</div>
 	</div>
+	<!-- /END THE FEATURETTES -->
 </div>
