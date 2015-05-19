2342372498042f899131d2fe63ff03525562b259
Rubén <rubencortesmunuera@gmail.com>
2015-05-19 10:55:14 +0200

    Editado (una vez más) el avatar de usuario para que no se cargue el menú superior. Cambiada mi foto por una seria Agregados ficheros de background general y logo de la página.

--- application/views/templates/menus/menu_logout.php
+++ application/views/templates/menus/menu_logout.php
@@ -39,7 +39,7 @@
 							<?php echo form_close()?>
 						</li>
 						<li class="dropdown">
-							<a href="#" class="dropdown-toggle img-responsive" data-toggle="dropdown">
+							<a href="#" class="dropdown-toggle img-responsive contenedor-avatar" data-toggle="dropdown">
 							     <?php echo img(array(
 							             'src' => asset_url(). '/images/users/'.$avatar,
 							             'class' => 'avatar'
