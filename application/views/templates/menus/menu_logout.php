5552f586e62223d71094997dae4c3fa4686bde1f
Mario <mcantelar@gmail.com>
2015-05-28 14:32:42 +0200

    desplegable listas creado

--- application/views/templates/menus/menu_logout.php
+++ application/views/templates/menus/menu_logout.php
@@ -21,7 +21,18 @@
 						<li class="<?php if ($title == 'Home') { echo 'active'; }?>"><?php echo anchor(base_url('home'), 'Home')?></li>
 						<li class="<?php if ($title == 'Libros') { echo 'active'; }?>"><?php echo anchor(base_url('libros'), 'Libros')?></li>
 						<li class="<?php if ($title == 'Autores') { echo 'active'; }?>"><?php echo anchor(base_url('autores'), 'Autores')?></li>
-						<li class="<?php if ($title == 'Lista de libros') { echo 'active'; }?>"><?php echo anchor(base_url('lista-libros'), 'Listas')?></li>
+												
+						<li class="dropdown <?php if ($title == 'Lista de libros' || $title == 'Lista de autores') echo "active" ?>">
+    						<a class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+        						Listas 
+                            </a>
+    						<ul class="dropdown-menu" role="menu">
+    							<li><?php echo anchor(base_url('lista-libros'), 'Lista de Libros')?></li>
+    							<li class="divider"></li>
+    							<li><?php echo anchor(base_url('lista-autores'), 'Lista de Autores')?></li>
+    						</ul>
+						</li>
+											
 					</ul>
 					<ul class="nav navbar-nav navbar-right">
 						<li>
