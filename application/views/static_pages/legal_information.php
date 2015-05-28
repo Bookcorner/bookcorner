0829086f4e0621bc4c79ba402d020efafea615ac
Antonio Ortiz <prueba@prueba.com>
2015-05-28 20:29:35 +0200

    Puesto paneles a la información legal

--- application/views/static_pages/legal_information.php
+++ application/views/static_pages/legal_information.php
@@ -9,132 +9,156 @@
 	<!-- FIN BREADCRUMB -->
 	<div class="row">
 		<div class="col-xs-12">
-			<h2 class="text-left">INFORMACIÓN LEGAL</h2>
-			<p>Bienvenido a la página Web de Bookcorner, S.A. Esta página Web
-				tiene por objeto facilitar al público en general la información
-				relativa a los productos y servicios de Bookcorner.</p>
-			<p>La utilización de la página Web atribuye la condición de usuario
-				de la página Web e implica la aceptación de todas las condiciones
-				incluidas en esta Información Legal. La prestación del servicio de
-				la página Web tiene una duración limitada al momento en el que el
-				usuario se encuentre conectado a la página Web o a alguno de los
-				servicios que a través de la misma se facilitan. Por tanto, el
-				usuario debe leer atentamente la presente Información Legal en cada
-				una de las ocasiones en que se proponga utilizar la página Web, ya
-				que ésta y sus condiciones de uso recogidas en la presente
-				Información Legal pueden sufrir modificaciones. Estas condiciones
-				generales se rigen por la ley española.</p>
+			<div class="panel">
+				<div class="panel-haeding">
+					<h2 class="lead text-center">INFORMACIÓN LEGAL</h2>
+				</div>
+				<div class="panel-body">
+					<p>Bienvenido a la página Web de Bookcorner, S.A. Esta página Web
+						tiene por objeto facilitar al público en general la información
+						relativa a los productos y servicios de Bookcorner.</p>
+					<p>La utilización de la página Web atribuye la condición de usuario
+						de la página Web e implica la aceptación de todas las condiciones
+						incluidas en esta Información Legal. La prestación del servicio de
+						la página Web tiene una duración limitada al momento en el que el
+						usuario se encuentre conectado a la página Web o a alguno de los
+						servicios que a través de la misma se facilitan. Por tanto, el
+						usuario debe leer atentamente la presente Información Legal en
+						cada una de las ocasiones en que se proponga utilizar la página
+						Web, ya que ésta y sus condiciones de uso recogidas en la presente
+						Información Legal pueden sufrir modificaciones. Estas condiciones
+						generales se rigen por la ley española.</p>
+				</div>
+			</div>
 		</div>
 	</div>
 	<div class="row">
 		<div class="col-xs-12">
-			<p class="lead">Advertencia Legal</p>
-			<ol>
-				<li><strong>BookCorner se reserva el derecho de modificar, en
-						cualquier momento y sin necesidad de previo aviso, la
-						configuración y contenidos de la página Web</strong></li>
-				<li>La información de esta página Web se proporciona sin garantía de
-					ninguna clase, ni explícita ni implícita, y podrá cambiarse o
-					actualizarse sin previo aviso.</li>
-				<li>El usuario se compromete a no llevar a cabo ninguna conducta que
-					pueda dañar los intereses y derechos de BookCorner o de terceros.
-					Toda la información que reciba BookCorner a través de este sitio
-					Web no se considerará confidencial, a excepción de la contemplada
-					en el apartado de confidencialidad de datos</li>
-				<li>Cualquier persona física o jurídica que quiera establecer un
-					hiperenlace o hipervínculo, necesitará una autorización previa de
-					BookCorner. BookCorner no asume responsabilidad alguna derivada de
-					los contenidos que terceras partes hayan incluido en los
-					hipervínculos a los que se hace referencia en esta página.</li>
-				<li>El establecimiento del hipervínculo no implica en ningún caso la
-					existencia de relaciones entre BookCorner y el propietario de la
-					página Web en la que se establezca, ni la aceptación y aprobación
-					por parte de BookCorner de sus contenidos o servicios allí
-					ofrecidos puestos a disposición del público. Queda expresamente
-					prohibido cualquier alteración, cambio, modificación o adaptación
-					de esta página Web o de cualquier información que la misma
-					contenga.</li>
-				<li><strong>BookCorner no se hace responsable de ningún daño o
-						perjuicio en el software o hardware del usuario que se derive del
-						acceso a su página Web o del uso de información o aplicaciones en
-						ella contenidas.</strong> Así mismo no se responsabiliza ni
-					garantiza la disponibilidad y continuidad en el acceso a esta
-					página Web o que el mismo esté libre de errores.</li>
-				<li>El usuario responderá de los daños y perjuicios de toda
-					naturaleza que BookCorner pueda sufrir como consecuencia del
-					incumplimiento de cualquiera de las obligaciones a las que queda
-					sometido por las presentes condiciones.</li>
-				<li>El usuario es consciente y acepta voluntariamente que el uso de
-					cualesquiera contenidos de esta página Web tiene lugar, en todo
-					caso, bajo su única y exclusiva responsabilidad.</li>
-			</ol>
+			<div class="panel">
+				<div class="panel-haeding">
+					<h3 class="lead"> 1. Advertencia Legal</h3>
+				</div>
+				<div class="panel-body">
+					<ol>
+						<li><strong>BookCorner se reserva el derecho de modificar, en
+								cualquier momento y sin necesidad de previo aviso, la
+								configuración y contenidos de la página Web</strong></li>
+						<li>La información de esta página Web se proporciona sin garantía
+							de ninguna clase, ni explícita ni implícita, y podrá cambiarse o
+							actualizarse sin previo aviso.</li>
+						<li>El usuario se compromete a no llevar a cabo ninguna conducta
+							que pueda dañar los intereses y derechos de BookCorner o de
+							terceros. Toda la información que reciba BookCorner a través de
+							este sitio Web no se considerará confidencial, a excepción de la
+							contemplada en el apartado de confidencialidad de datos</li>
+						<li>Cualquier persona física o jurídica que quiera establecer un
+							hiperenlace o hipervínculo, necesitará una autorización previa de
+							BookCorner. BookCorner no asume responsabilidad alguna derivada
+							de los contenidos que terceras partes hayan incluido en los
+							hipervínculos a los que se hace referencia en esta página.</li>
+						<li>El establecimiento del hipervínculo no implica en ningún caso
+							la existencia de relaciones entre BookCorner y el propietario de
+							la página Web en la que se establezca, ni la aceptación y
+							aprobación por parte de BookCorner de sus contenidos o servicios
+							allí ofrecidos puestos a disposición del público. Queda
+							expresamente prohibido cualquier alteración, cambio, modificación
+							o adaptación de esta página Web o de cualquier información que la
+							misma contenga.</li>
+						<li><strong>BookCorner no se hace responsable de ningún daño o
+								perjuicio en el software o hardware del usuario que se derive
+								del acceso a su página Web o del uso de información o
+								aplicaciones en ella contenidas.</strong> Así mismo no se
+							responsabiliza ni garantiza la disponibilidad y continuidad en el
+							acceso a esta página Web o que el mismo esté libre de errores.</li>
+						<li>El usuario responderá de los daños y perjuicios de toda
+							naturaleza que BookCorner pueda sufrir como consecuencia del
+							incumplimiento de cualquiera de las obligaciones a las que queda
+							sometido por las presentes condiciones.</li>
+						<li>El usuario es consciente y acepta voluntariamente que el uso
+							de cualesquiera contenidos de esta página Web tiene lugar, en
+							todo caso, bajo su única y exclusiva responsabilidad.</li>
+					</ol>
+				</div>
+			</div>
 
-			<p class="lead">Política de Privacidad</p>
-			<ol>
-				<li><strong>BookCorner se compromete a respetar lo dispuesto en la
-						Ley Orgánica 15/1999 de 13 de diciembre, de protección de datos de
-						carácter personal y, en este sentido, informa de lo que sigue.</strong></li>
-				<li>Para la utilización de determinados contenidos y servicios,
-					BookCorner podrá requerir del usuario ciertos datos de carácter
-					personal. En tal caso, BookCorner informará, con carácter previo y
-					adecuadamente al usuario a fin de que éste pueda prestar su
-					consentimiento al tratamiento de sus datos por parte de BookCorner
-					para la finalidad concreta. En tales casos, determinados datos se
-					consideran imprescindibles y, si los mismos no se facilitan, no
-					será posible la utilización de dichos contenidos o servicios.
-					Dichos datos se mantendrán en la más estricta confidencialidad y no
-					se utilizarán para ningún otro fin.</li>
-				<li>El usuario podrá revocar el consentimiento prestado y ejercer
-					los derechos de acceso, rectificación, cancelación y oposición
-					dirigiéndose a tal efecto a la siguiente dirección: Departamento de
-					Relaciones Laborales de BookCorner SISTEMAS, S.A. (Sede Central:
-					Avenida de Bruselas, 33-35, 28108, Alcobendas-Madrid). Este sitio
-					utiliza las herramientas de medición de audiencia de RedSheriff,
-					que permiten analizar el comportamiento y perfil de los visitantes
-					anónimos, con el objetivo de mejorar el funcionamiento del sitio,
-					esta medición utiliza cookies de tercera parte. El usuario puede
-					acceder a la política de privacidad de esta empresa en esta
-					dirección.</li>
-			</ol>
+			<div class="panel">
+				<div class="panel-haeding">
+					<h3 class="lead"> 2. Política de Privacidad</h3>
+				</div>
+				<div class="panel-body">
+					<ol>
+						<li><strong>BookCorner se compromete a respetar lo dispuesto en la
+								Ley Orgánica 15/1999 de 13 de diciembre, de protección de datos
+								de carácter personal y, en este sentido, informa de lo que
+								sigue.</strong></li>
+						<li>Para la utilización de determinados contenidos y servicios,
+							BookCorner podrá requerir del usuario ciertos datos de carácter
+							personal. En tal caso, BookCorner informará, con carácter previo
+							y adecuadamente al usuario a fin de que éste pueda prestar su
+							consentimiento al tratamiento de sus datos por parte de
+							BookCorner para la finalidad concreta. En tales casos,
+							determinados datos se consideran imprescindibles y, si los mismos
+							no se facilitan, no será posible la utilización de dichos
+							contenidos o servicios. Dichos datos se mantendrán en la más
+							estricta confidencialidad y no se utilizarán para ningún otro
+							fin.</li>
+						<li>El usuario podrá revocar el consentimiento prestado y ejercer
+							los derechos de acceso, rectificación, cancelación y oposición
+							dirigiéndose a tal efecto a la siguiente dirección: Departamento
+							de Relaciones Laborales de BookCorner SISTEMAS, S.A. (Sede
+							Central: Avenida de Bruselas, 33-35, 28108, Alcobendas-Madrid).
+							Este sitio utiliza las herramientas de medición de audiencia de
+							RedSheriff, que permiten analizar el comportamiento y perfil de
+							los visitantes anónimos, con el objetivo de mejorar el
+							funcionamiento del sitio, esta medición utiliza cookies de
+							tercera parte. El usuario puede acceder a la política de
+							privacidad de esta empresa en esta dirección.</li>
+					</ol>
+				</div>
+			</div>
 
-			<p class="lead">Copyright</p>
-			<ol>
-				<li><strong>Todos los derechos sobre el software de la página Web
-						así como de los derechos de propiedad industrial e intelectual
-						referidos a los contenidos que en ella se incluyan son propiedad
-						de BookCorner o de terceros.</strong></li>
-				<li>Esta página Web puede contener referencias a marcas comerciales
-					o marcas registradas de BookCorner. Queda prohibido su uso sin el
-					consentimiento previo y expreso de BookCorner. Todas las demás
-					referencias a otras marcas comerciales o registradas pertenecen a
-					sus respectivos propietarios, sin que pueda entenderse que el
-					acceso a la página Web atribuya derecho alguno sobre las mismas.
-					Cualquier persona está autorizada para ver, copiar, imprimir y
-					distribuir cualquier documento publicado por BookCorner en esta
-					página Web teniendo en cuenta que:
-					<ul>
-						<li>(i) dicho documento será utilizado exclusivamente con fines
-							informativos</li>
-						<li>(ii) dicho documento no podrá ser utilizado con fines
-							comerciales</li>
-						<li>(iii) dicho documento deberá siempre incluir referencia de que
-							es propiedad de BookCorner.</li>
-					</ul>
-				</li>
-				<li>Los materiales de esta página Web están protegidos por derechos
-					de autor y cualquier uso no autorizado de los mismos puede violar
-					la normativa sobre derechos de autor, marcas y otras regulaciones.
-					La autorización concedida para utilizar esta página Web se
-					entenderá automáticamente terminada en caso de infringir cualquiera
-					de estas Condiciones, estando obligado a destruir inmediatamente
-					cualquier material obtenido o impreso de la página Web. BookCorner
-					no concede ninguna licencia o autorización de uso de ninguna clase
-					sobre sus derechos de propiedad industrial e intelectual, secretos
-					empresariales o sobre cualquier otra propiedad o derecho
-					relacionado con la página Web y sus contenidos.</li>
-		
+			<div class="panel">
+				<div class="panel-haeding">
+					<h3 class="lead"> 3. Copyright</h3>
+				</div>
+				<div class="panel-body">
+					<ol>
+						<li><strong>Todos los derechos sobre el software de la página Web
+								así como de los derechos de propiedad industrial e intelectual
+								referidos a los contenidos que en ella se incluyan son propiedad
+								de BookCorner o de terceros.</strong></li>
+						<li>Esta página Web puede contener referencias a marcas
+							comerciales o marcas registradas de BookCorner. Queda prohibido
+							su uso sin el consentimiento previo y expreso de BookCorner.
+							Todas las demás referencias a otras marcas comerciales o
+							registradas pertenecen a sus respectivos propietarios, sin que
+							pueda entenderse que el acceso a la página Web atribuya derecho
+							alguno sobre las mismas. Cualquier persona está autorizada para
+							ver, copiar, imprimir y distribuir cualquier documento publicado
+							por BookCorner en esta página Web teniendo en cuenta que:
+							<ul>
+								<li>(i) dicho documento será utilizado exclusivamente con fines
+									informativos</li>
+								<li>(ii) dicho documento no podrá ser utilizado con fines
+									comerciales</li>
+								<li>(iii) dicho documento deberá siempre incluir referencia de
+									que es propiedad de BookCorner.</li>
+							</ul>
+						</li>
+						<li>Los materiales de esta página Web están protegidos por
+							derechos de autor y cualquier uso no autorizado de los mismos
+							puede violar la normativa sobre derechos de autor, marcas y otras
+							regulaciones. La autorización concedida para utilizar esta página
+							Web se entenderá automáticamente terminada en caso de infringir
+							cualquiera de estas Condiciones, estando obligado a destruir
+							inmediatamente cualquier material obtenido o impreso de la página
+							Web. BookCorner no concede ninguna licencia o autorización de uso
+							de ninguna clase sobre sus derechos de propiedad industrial e
+							intelectual, secretos empresariales o sobre cualquier otra
+							propiedad o derecho relacionado con la página Web y sus
+							contenidos.</li>
+					</ol>
+				</div>
+			</div>
 		</div>
-	</div>
-</div>
-<br>
-<br>
+		<br> <br>
