873d3f957b1b5ce731fa1fbba340dbdb7ebecfc8
Antonio Ortiz <prueba@prueba.com>
2015-06-02 16:14:20 +0200

    Cambio en la vista de la página de administración

--- application/views/admin/adminhome.php
+++ application/views/admin/adminhome.php
@@ -9,33 +9,130 @@
 	<!-- FIN BREADCRUMB -->
 	<!-- CONTENIDO PPAL-->
 	<div class="row">
-		<div class="col-xs-9">
-            <div class="panel">
-					<div class="panel-heading">
-						<h3>
-							<i class="icon-time main-color"></i> Administración
-						</h3>
-					</div>
-					<div class="panel-body">
-					    <div>
-    						<h2 class="lead">Instrucciones</h2>					    
-    						<p>Escoge una table en la lista de la derecha para editar el CRUD</p>
-					    </div>
-					    <div>
-    						<h2 class="lead">Datos estadísticos sobre la página</h2>					    
-    						<p>Número de usuarios, número de libros, número de autores, número de votos, número de libros en todas las listbooks...</p>
-					    </div>
-					</div>
-				</div>
+		<div class="col-xs-8">
+   			<div class="row">
+   				<div class="col-xs-6">
+   					<div class="panel panel-primary">
+   						<div class="panel-heading">
+   							<div class="row">
+   								<div class="col-xs-3">
+   									<i class="fa fa-users fa-5x"></i>
+   								</div>
+   								<div class="col-xs-9 text-right">
+   									<div class="huge">26</div>
+   									<div>Usuarios!</div>
+   								</div>
+   							</div>
+   						</div>
+   						<a href="<?php echo base_url('adminusers')?>">
+   							<div class="panel-footer">
+   								<span class="pull-left">Ver Detalles</span> <span
+   									class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+   								<div class="clearfix"></div>
+   							</div>
+   						</a>
+   					</div>
+   				</div>
+   				<div class="col-xs-6">
+   					<div class="panel panel-success">
+   						<div class="panel-heading">
+   							<div class="row">
+   								<div class="col-xs-3">
+   									<i class="fa fa-book fa-5x"></i>
+   								</div>
+   								<div class="col-xs-9 text-right">
+   									<div class="huge">123</div>
+   									<div>Libros!</div>
+   								</div>
+   							</div>
+   						</div>
+  						<a href="<?php echo base_url('adminlistbook')?>">
+   							<div class="panel-footer">
+   								<span class="pull-left">Ver Detalles</span> <span
+   									class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+   								<div class="clearfix"></div>
+   							</div>
+   						</a>
+   					</div>
+   				</div>
+       		</div>
+   	    	<div class="row">
+    			<div class="col-xs-6">
+    				<div class="panel panel-info">
+    					<div class="panel-heading">
+    						<div class="row">
+    							<div class="col-xs-3">
+    								<i class="fa fa-pencil-square-o fa-5x"></i>
+    							</div>
+    							<div class="col-xs-9 text-right">
+    								<div class="huge">124</div>
+    								<div>Autores!</div>
+    							</div>
+    						</div>
+    					</div>
+    					<a href="<?php echo base_url('adminauthors')?>">
+    						<div class="panel-footer">
+    							<span class="pull-left">Ver Detalles</span> <span
+    								class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+    							<div class="clearfix"></div>
+    						</div>
+    					</a>
+    				</div>
+    			</div>
+    			<div class="col-xs-6">
+    				<div class="panel panel-warning">
+    					<div class="panel-heading">
+    						<div class="row">
+    							<div class="col-xs-6">
+    								<i class="fa fa-female fa-5x"></i>
+    								<i class="fa fa-male fa-5x"></i>
+    							</div>
+    							<div class="col-xs-6 text-right">
+    								<div class="huge">300</div>
+    								<div>Géneros Literarios!</div>
+    							</div>
+    						</div>
+    					</div>
+    					<a href="<?php echo base_url('admingenre')?>">
+    						<div class="panel-footer">
+    							<span class="pull-left">Ver Detalles</span> <span
+    								class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+    							<div class="clearfix"></div>
+    						</div>
+    					</a>
+    				</div>
+    			</div>
+    		</div>
 		</div>
-		<div class="col-xs-3">
-			<ul class="list-group">
-				<li class="list-group-item"><?php echo anchor(base_url('adminusers'), 'Administración Usuarios')?></li>
-				<li class="list-group-item"><?php echo anchor(base_url('adminlistbook'), 'Administración Lista de Libros')?></li>
-				<li class="list-group-item"><?php echo anchor(base_url('adminauthors'), 'Administración Autores')?></li>
-				<li class="list-group-item"><?php echo anchor(base_url('adminbooks'), 'Administración Libros')?></li>
-				<li class="list-group-item"><?php echo anchor(base_url('admingenre'), 'Administración Género')?></li>
-			</ul>
+		<div class="col-xs-4">
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					<i class="fa fa-bell fa-fw"></i> Administración Tablas
+				</div>
+				<!-- /.panel-heading -->
+				<div class="panel-body">
+					<div class="list-group">
+						<a href="<?php echo base_url('adminusers')?>"
+							class="list-group-item"> <i class="fa fa-users"></i>
+							Administración Usuarios
+						</a> 
+						<a href="<?php echo base_url('adminlistbook')?>" class="list-group-item"> 
+						  <i class="fa fa-list"></i> Administración Lista de Libros
+						</a> 
+						<a href="<?php echo base_url('adminauthors')?>" class="list-group-item"> 
+						  <i class="fa fa-pencil-square-o"></i> Administración Autores
+						</a> 
+						<a href="<?php echo base_url('adminbooks')?>" class="list-group-item"> 
+						  <i class="fa fa-book"></i> Administración Libros
+						</a> 
+						<a href="<?php echo base_url('admingenre')?>" class="list-group-item"> 
+						  <i class="fa fa-female"></i><i class="fa fa-male"></i>  Administración Géneros Literarios
+						</a>
+					</div>
+					<!-- /.list-group -->
+				</div>
+				<!-- /.panel-body -->
+			</div>
 		</div>
 	</div>
 </div>
