95beb4ce4c27ee12aff95204fd9280d0d2e66fba
Antonio Ortiz <prueba@prueba.com>
2015-06-06 22:40:58 +0200

    Cambiada la tabla de administración para que se vea en dispositivos de menor resolución

--- application/views/admin/adminview.php
+++ application/views/admin/adminview.php
@@ -14,7 +14,8 @@
 				<div class="panel-heading">
 					<i class="fa fa-database"fa-fw"></i> Administración Tablas
 				</div>
-				<div class="panel-body">
+				<!-- BODY LG -->
+				<div class="panel-body visible-lg">
 					<div class="row">
 						<div class="col-xs-2">
 							<a href="<?php echo base_url('adminusers')?>">
@@ -92,6 +93,240 @@
 											<div class="col-xs-6 text-right">
 												<div>Géneros Literarios</div>
 											</div>
+										</div>
+									</div>
+								</div>
+							</a>
+						</div>
+					</div>
+					<!-- /.panel-body -->
+				</div>	
+					<!-- BODY RESTO -->
+				<div class="panel-body visible-md">
+					<div class="row">
+						<div class="col-xs-2">
+							<a href="<?php echo base_url('adminusers')?>">
+								<div class="panel panel-primary">
+									<div class="panel-heading">
+										<div class="row">
+											<div class="col-xs-3">
+												<i class="fa fa-users fa-5x"></i>
+											</div>
+											<div class="col-xs-9 text-right"></div>
+										</div>
+									</div>
+								</div>
+							</a>
+						</div>
+						<div class="col-xs-2">
+							<a href="<?php echo base_url('adminlistbook')?>">
+								<div class="panel panel-danger">
+									<div class="panel-heading">
+										<div class="row">
+											<div class="col-xs-3">
+												<i class="fa fa-list fa-5x"></i>
+											</div>
+											<div class="col-xs-9 text-right"></div>
+										</div>
+									</div>
+								</div>
+							</a>
+						</div>
+						<div class="col-xs-2">
+							<a href="<?php echo base_url('adminauthors')?>">
+								<div class="panel panel-info">
+									<div class="panel-heading">
+										<div class="row">
+											<div class="col-xs-3">
+												<i class="fa fa-pencil-square-o fa-5x"></i>
+											</div>
+											<div class="col-xs-9 text-right"></div>
+										</div>
+									</div>
+								</div>
+							</a>
+						</div>
+						<div class="col-xs-2">
+							<a href="<?php echo base_url('adminbooks')?>">
+								<div class="panel panel-success">
+									<div class="panel-heading">
+										<div class="row">
+											<div class="col-xs-3">
+												<i class="fa fa-book fa-5x"></i>
+											</div>
+											<div class="col-xs-9 text-right"></div>
+										</div>
+									</div>
+								</div>
+							</a>
+						</div>
+						<div class="col-xs-4">
+							<a href="<?php echo base_url('admingenre')?>">
+								<div class="panel panel-warning">
+									<div class="panel-heading">
+										<div class="row">
+											<div class="col-xs-6">
+												<i class="fa fa-female fa-5x"></i> <i
+													class="fa fa-male fa-5x"></i>
+											</div>
+											<div class="col-xs-6 text-right">
+                                                
+												<div>Géneros Literarios</div> 
+											</div>
+										</div>
+									</div>
+								</div>
+							</a>
+						</div>
+					</div>
+					<!-- /.panel-body -->
+				</div>
+					<!-- BODY SM -->
+				<div class="panel-body visible-sm">
+					<div class="row">
+						<div class="col-xs-2">
+							<a href="<?php echo base_url('adminusers')?>">
+								<div class="panel panel-primary">
+									<div class="panel-heading">
+										<div class="row">
+											<div class="col-xs-3">
+												<i class="fa fa-users fa-3x"></i>
+											</div>
+											<div class="col-xs-9 text-right"></div>
+										</div>
+									</div>
+								</div>
+							</a>
+						</div>
+						<div class="col-xs-2">
+							<a href="<?php echo base_url('adminlistbook')?>">
+								<div class="panel panel-danger">
+									<div class="panel-heading">
+										<div class="row">
+											<div class="col-xs-3">
+												<i class="fa fa-list fa-3x"></i>
+											</div>
+											<div class="col-xs-9 text-right"></div>
+										</div>
+									</div>
+								</div>
+							</a>
+						</div>
+						<div class="col-xs-2">
+							<a href="<?php echo base_url('adminauthors')?>">
+								<div class="panel panel-info">
+									<div class="panel-heading">
+										<div class="row">
+											<div class="col-xs-3">
+												<i class="fa fa-pencil-square-o fa-3x"></i>
+											</div>
+											<div class="col-xs-9 text-right"></div>
+										</div>
+									</div>
+								</div>
+							</a>
+						</div>
+						<div class="col-xs-2">
+							<a href="<?php echo base_url('adminbooks')?>">
+								<div class="panel panel-success">
+									<div class="panel-heading">
+										<div class="row">
+											<div class="col-xs-3">
+												<i class="fa fa-book fa-3x"></i>
+											</div>
+											<div class="col-xs-9 text-right"></div>
+										</div>
+									</div>
+								</div>
+							</a>
+						</div>
+						<div class="col-xs-4">
+							<a href="<?php echo base_url('admingenre')?>">
+								<div class="panel panel-warning">
+									<div class="panel-heading">
+										<div class="row">
+											<div class="col-xs-6">
+												<i class="fa fa-female fa-3x"></i> <i
+													class="fa fa-male fa-3x"></i>
+											</div>
+											<div class="col-xs-6 text-right"></div>
+										</div>
+									</div>
+								</div>
+							</a>
+						</div>
+					</div>
+					<!-- /.panel-body -->
+				</div>
+					<!-- BODY XS -->
+				<div class="panel-body visible-xs">
+					<div class="row">
+						<div class="col-xs-2">
+							<a href="<?php echo base_url('adminusers')?>">
+								<div class="panel panel-primary">
+									<div class="panel-heading">
+										<div class="row">
+											<div class="col-xs-3">
+												<i class="fa fa-users fa-2x"></i>
+											</div>
+											<div class="col-xs-9 text-right"></div>
+										</div>
+									</div>
+								</div>
+							</a>
+						</div>
+						<div class="col-xs-2">
+							<a href="<?php echo base_url('adminlistbook')?>">
+								<div class="panel panel-danger">
+									<div class="panel-heading">
+										<div class="row">
+											<div class="col-xs-3">
+												<i class="fa fa-list fa-2x"></i>
+											</div>
+											<div class="col-xs-9 text-right"></div>
+										</div>
+									</div>
+								</div>
+							</a>
+						</div>
+						<div class="col-xs-2">
+							<a href="<?php echo base_url('adminauthors')?>">
+								<div class="panel panel-info">
+									<div class="panel-heading">
+										<div class="row">
+											<div class="col-xs-3">
+												<i class="fa fa-pencil-square-o fa-2x"></i>
+											</div>
+											<div class="col-xs-9 text-right"></div>
+										</div>
+									</div>
+								</div>
+							</a>
+						</div>
+						<div class="col-xs-2">
+							<a href="<?php echo base_url('adminbooks')?>">
+								<div class="panel panel-success">
+									<div class="panel-heading">
+										<div class="row">
+											<div class="col-xs-3">
+												<i class="fa fa-book fa-2x"></i>
+											</div>
+											<div class="col-xs-9 text-right"></div>
+										</div>
+									</div>
+								</div>
+							</a>
+						</div>
+						<div class="col-xs-4">
+							<a href="<?php echo base_url('admingenre')?>">
+								<div class="panel panel-warning">
+									<div class="panel-heading">
+										<div class="row">
+											<div class="col-xs-6">
+												<i class="fa fa-female fa-2x"></i> <i
+													class="fa fa-male fa-2x"></i>
+											</div>
+											<div class="col-xs-6 text-right"></div>
 										</div>
 									</div>
 								</div>
