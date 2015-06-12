e7a28ff2a5cfe1fed62a65a63c940bcf2808e7c6
rubcortes <rubencortesmunuera@gmail.com>
2015-06-12 17:19:07 +0200

    cambio en la vista del movil del menú de logout y de la las listas.

--- application/views/templates/menus/menu_logout.php
+++ application/views/templates/menus/menu_logout.php
@@ -1,5 +1,6 @@
 <div class="container">
-	<nav class="navbar navbar-default navbar-inverse">
+    <!-- INICIO MENU ESCRITORIO -->
+	<nav class="navbar navbar-default navbar-inverse hidden-xs">
 		<div class="container-fluid">
             <!-- IMAGEN -->
 			<div class="navbar-header visible-lg visible-md">
@@ -9,12 +10,6 @@
                           'class' => 'Brand logo-lg-md' 
                     ) )?>
 				</a>
-				<button type="button" class="navbar-toggle collapsed"
-					data-toggle="collapse" data-target="#idFormAndLogin">
-					<span class="sr-only">Toggle navigation</span> <span
-						class="icon-bar"></span> <span class="icon-bar"></span> <span
-						class="icon-bar"></span>
-				</button>
 			</div>
 			<div class="navbar-header visible-sm no-padding col-sm-1">
 				<a class="navbar-brand no-padding" href="<?= base_url('que-es-bookcorner')?>"> 
@@ -23,12 +18,6 @@
                           'class' => 'Brand logo-sm' 
                     ) )?>
 				</a>
-				<button type="button" class="navbar-toggle collapsed"
-					data-toggle="collapse" data-target="#idFormAndLogin">
-					<span class="sr-only">Toggle navigation</span> <span
-						class="icon-bar"></span> <span class="icon-bar"></span> <span
-						class="icon-bar"></span>
-				</button>
 			</div>
 			<!-- FIN IMAGEN -->
 			<!-- OPCIONES MENU -->
@@ -130,8 +119,7 @@
     				</div>
         		<?php echo form_close()?>
         		<!-- FIN BUSCADOR -->
-        		<!-- REGISTRO -->
-                    <!-- TODOS -->
+                    <!-- PERFIL -->
                     <ul class="nav navbar-nav navbar-right">
                         <li class="dropdown">
 							<a href="#" class="dropdown-toggle img-responsive contenedor-avatar" data-toggle="dropdown">
@@ -187,8 +175,132 @@
 							</ul>
 						</li>
     				</ul>
-    			<!-- REGISTRO -->
+    			<!-- PERFIL -->
 			</div>
 		</div>
 	</nav>
+	<!-- FIN MENU ESCRITORIO -->
+	<!-- INICIO MENU MOVILES -->
+	<nav class="navbar navbar-inverse visible-xs">
+		<div class="container-fluid">
+			<!-- Brand and toggle get grouped for better mobile display -->
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed"
+					data-toggle="collapse" data-target="#idMenuContent">
+					<span class="sr-only">Toggle navigation</span> <span
+						class="icon-bar"></span> <span class="icon-bar"></span> <span
+						class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="<?= base_url('home')?>">
+				    <?php echo img ( array (
+                          'src' => asset_url () . '/images/logo/bc.png',
+                          'class' => 'Brand logo-lg-md' 
+                    ) )?>
+				</a>
+			</div>
+
+			<!-- Collect the nav links, forms, and other content for toggling -->
+			<div class="collapse navbar-collapse" id="idMenuContent">
+				<ul class="nav navbar-nav">
+					<li class="<?php if ($title == 'Home') { echo 'active'; }?>"><a
+						href="<?php echo base_url('home')?>"> <i class="fa fa-home"></i>
+							Home
+					</a></li>
+					<li class="<?php if ($title == 'Libros') { echo 'active'; }?>"><a
+						href="<?php echo base_url('libros')?>"> <i class="fa fa-book"></i>
+							Libros
+					</a></li>
+					<li class="<?php if ($title == 'Autores') { echo 'active'; }?>"><a
+						href="<?php echo base_url('autores')?>"> <i class="fa fa-user"></i>
+							Autores
+					</a></li>
+					<li class="<?php if ($title == 'Lista de libros') { echo 'active'; }?>">
+    					<a href="<?php echo base_url('lista-libros')?>">
+        					<i class="fa fa-list-alt"></i>
+    						Lista
+    					</a>
+				    </li>
+				</ul>
+				<?php
+                echo form_open ( 'busqueda', [ 
+                        'id' => 'idSearchForm',
+                        'class' => 'navbar-form navbar-left overflow',
+                        'role' => 'search' 
+                ] )?>
+                <div class="col-xs-4">
+                    <select class="form-control" name="typeOfSearch">
+    					<option value="book">Libros</option>
+    					<option value="author">Autores</option>
+    				</select>
+                </div>
+                <div class="form-group col-xs-6">
+					<input type="text" id="idSearchName" name="searchName"
+						class="form-control" placeholder="Buscar...">
+				</div>
+				<button type="submit" class="btn btn-default col-xs-2">
+					<i class="glyphicon glyphicon-search"></i>
+				</button>
+        		<?php echo form_close()?>
+				<!-- PERFIL -->
+                    <ul class="nav navbar-nav navbar-right">
+                        <li class="dropdown">
+							<a href="#" class="dropdown-toggle img-responsive contenedor-avatar" data-toggle="dropdown">
+							     <?php echo img(array(
+							             'src' => asset_url(). '/images/users/'.$avatar,
+							             'class' => 'avatar'
+							     ))?>
+							</a>
+							<ul class="dropdown-menu" style="padding: 15px; min-width: 150px;">
+								<li role="presentation" class="dropdown-header"><?php echo "$username $surname"?></li>
+								<li role="presentation">
+								    <a href="<?php echo base_url('informacion-de-usuario')?>" role="menuitem">
+								        <i class="fa fa-user"></i>
+								        Perfil
+								    </a>
+								</li>
+								<li role="presentation">
+								    <a href="<?php echo base_url('reportes')?>" role="menuitem">
+								        <i class="fa fa-ambulance"></i>
+								        Reportes
+								    </a>
+								</li>
+								<li role="presentation">
+								    <a href="<?php echo base_url('ver-votos')?>" role="menuitem">
+								        <i class="fa fa-thumbs-o-up"></i> Votaciones 
+								    </a>
+								</li>
+								<li role="presentation">
+								    <?php if ($role == 2 || $role == 3):?>
+								    <a href="<?php echo base_url('moderar')?>" role="menuitem">
+								        <i class="fa fa-eye"></i>
+								        Moderar
+								        <span class="badge"><?php echo $number_of_reports?></span>
+								    </a>
+								    <?php endif;?>
+								</li>
+															
+								<li role="presentation" class="divider"></li>
+								<?php if ($role == 3) { ?>
+								<li role="presentation">
+								    <a href="<?php echo base_url('Admin')?>">
+								        <i class="fa fa-cog fa-spin"></i>
+								        Admin
+								    </a>
+								</li>
+								<?php } ?>
+								<li>
+								    <a href="<?php echo base_url('salir')?>">
+								        <i class="fa fa-sign-out"></i>
+								        Salir
+								    </a>
+                                </li>
+							</ul>
+						</li>
+    				</ul>
+    			<!-- PERFIL -->
+			</div>
+		</div>
+	</nav>
+	<!-- FIN MENU MOVILES -->
+	
 </div>
